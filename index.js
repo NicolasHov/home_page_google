d5fe3b8888e3203015e564dcf0c6889074ccfbb0
NicolasHov <kola.hov@gmail.com>
2017-10-04 12:55:20 +0200

    Added audio.png index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+// initialisation
+  // show button + hide body
+  $(".button-initialize").click(function(){
+      $(body).hide();
+      $("#div1").fadeIn();
+      $("#div2").fadeIn("slow");
+      $("#div3").fadeIn(3000);
+  });
+  
+  $(".btn").
+
+// on button click : hide button + show body
+
+});
